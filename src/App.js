638de8c83873f61a111603b00a86558da0ab2b9d
sarangi-ankit <sarangiankit097@gmail.com>
2021-04-16 17:39:52 +0530

    final setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component{
+        state={todos:[]}; 
+        constructor(){
+        super()
+        this.state={todos: []};
+    
+    }
+
+    addList=()=>{
+        Axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then((response)=>{
+           this.setState({todos:response.data.slice(0,40)})
+        })
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>To Do-app</h1>
+                <button className="list" onClick={this.addList}>Fetch</button>
+                 {this.state.todos.length>0?(
+                    <div>
+                        <ul>
+                            {this.state.todos.map(todo=>(
+                            <li key={todo.id}>
+                                {todo.title}
+                                <span className="marker">
+                                    <input className="checked-box" type="checkbox" checked={todo.completed} />
+                                </span>
+
+                            </li>
+                            )
+
+                            )}
+                        </ul>
+                        
+                    </div>
+                ):(
+                    <div>
+                        
+                    </div>
+                )
+            }
+            </div> 
+        )
+    }
 }
-
 export default App;
